6b7dfa13035e317013b300f7b5fabbb5d8b189fc
areekaras <shibili.areekara@gmail.com>
2022-01-20 19:59:11 +0530

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Shibili Areekara on 20/01/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    var description: String?
+    var location: String?
+    var url: URL
+}
+
