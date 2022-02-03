80b41ca5112bf393177b798652a519bde493d601
areekaras <shibili.areekara@gmail.com>
2022-02-03 12:00:12 +0530

    Move 'RemoteFeedItem' to its own file

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Shibili Areekara on 03/02/22.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
