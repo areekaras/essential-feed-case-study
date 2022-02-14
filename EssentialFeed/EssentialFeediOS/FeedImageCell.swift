f5c004704dbcd83d814bf44f015cdfcb9353a3ba
areekaras <shibili.areekara@gmail.com>
2022-02-14 11:07:42 +0530

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Shibili Areekara on 14/02/22.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let descriptionLabel = UILabel()
+    public let locationLabel = UILabel()
+}
