a8143966c4ada7f22f6e366685fcfed3f8a0e632
areekaras <shibili.areekara@gmail.com>
2022-02-20 12:11:40 +0530

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Shibili Areekara on 20/02/22.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
