d81fd7fdb74bf3d305acf9bb61e325bafd5076f4
areekaras <shibili.areekara@gmail.com>
2023-01-08 12:50:13 +0530

    Improve coverage by using identifiers for finding feed image cells

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -14,7 +14,10 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
         
         app.launch()
         
-        XCTAssertEqual(app.cells.count, 22)
-//        XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+        let cells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(cells.count, 22)
+        
+        let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstImage.exists)
     }
 }
